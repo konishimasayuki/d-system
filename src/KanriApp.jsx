c1fc97e1544d757cb75fe0f7bffdada628ba5ed7
konishimasayuki <ninjin.konishi@gmail.com>
2026-08-24 21:44:58 +0900

    Update KanriApp.jsx

--- src/KanriApp.jsx
+++ src/KanriApp.jsx
@@ -196,7 +196,7 @@ export default function KanriApp() {
           {tab === "driverpage" && <DriverPage reservations={reservations} casts={casts} drivers={drivers} />}
           {tab === "mypage" && <CastMyPage casts={casts} reservations={reservations} />}
           {tab === "std" && <StdManagement casts={casts} />}
-          {tab === "uketsuke" && <UketsukeTab casts={casts} courses={courses} drivers={drivers} />}
+          {tab === "uketsuke" && <UketsukeTab casts={casts} courses={courses} options={options} drivers={drivers} />}
           {tab === "driverschedule" && <DriverScheduleTab drivers={drivers} />}
           {tab === "settings" && <SettingsTab setCasts={setCasts} drivers={drivers} setDrivers={setDrivers} hotels={hotels} setHotels={setHotels} office={office} setOffice={setOffice} staff={staff} setStaff={setStaff} courses={courses} setCourses={setCourses} options={options} setOptions={setOptions} setReservations={setReservations} syncMsg={syncMsg} />}
         </div>
